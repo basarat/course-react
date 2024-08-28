9190a14f85cb289c38045ba9f859f9b5ec63ac7f
basarat <basaratali@gmail.com>
2024-08-28 14:56:43 +1000

    style

---
+++ lessons/styling_style/src/App.tsx
@@ -0,0 +1,20 @@
+const red = {
+  color: 'red',
+};
+
+const blue = {
+  color: 'blue',
+};
+
+export default function App() {
+  return (
+    <>
+      <div style={red}>
+        Hello Fam!
+      </div>
+      <div style={blue}>
+        Hope you have a great day!
+      </div>
+    </>
+  );
+}
