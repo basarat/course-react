5c0c0197e339dbb14e1b279f82e356f29952241a
basarat <basaratali@gmail.com>
2024-08-28 13:21:54 +1000

    styling css

---
+++ lessons/styling_css/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <>
+      <div className='red'>
+        Hello Fam!
+      </div>
+      <div className='blue'>
+        Hope you have a great day!
+      </div>
+    </>
+  );
+}
