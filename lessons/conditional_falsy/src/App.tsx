4b540a9946e026418d60463f94fde3c159f76793
basarat <basaratali@gmail.com>
2024-08-28 18:26:14 +1000

    falsy

---
+++ lessons/conditional_falsy/src/App.tsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <>
+      <div>false: {false}</div>
+      <div>null: {null}</div>
+      <div>undefined: {undefined}</div>
+      <div>'': {""}</div>
+      <div>0: {0}</div>
+      <div>NaN: {NaN}</div>
+    </>
+  );
+}
