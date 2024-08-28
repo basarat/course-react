6689538ae8c0cb8652a8d9854b2c2efec7277b4b
basarat <basaratali@gmail.com>
2024-08-28 13:11:37 +1000

    fragments_explicit

---
+++ lessons/fragments_explicit/src/App.tsx
@@ -0,0 +1,14 @@
+import { Fragment } from 'react';
+
+export default function App() {
+  return (
+    <Fragment>
+      <div>
+        Hello Fam!
+      </div>
+      <div>
+        How are you today?
+      </div>
+    </Fragment>
+  );
+}
