16e0432486c5d796463bc862f21795798b3aaf47
basarat <basaratali@gmail.com>
2024-08-28 15:03:18 +1000

    css modules

---
+++ lessons/styling_css_modules/src/App.tsx
@@ -0,0 +1,14 @@
+import styles from './App.module.css';
+
+export default function App() {
+  return (
+    <>
+      <div className={styles.red}>
+        Hello Fam!
+      </div>
+      <div className={styles.blue}>
+        Hope you have a great day!
+      </div>
+    </>
+  );
+}
