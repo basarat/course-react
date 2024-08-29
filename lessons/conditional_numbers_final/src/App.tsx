777788a9805a427e25badb1698ab8decb50cae0a
basarat <basaratali@gmail.com>
2024-08-29 10:23:14 +1000

    another

---
+++ lessons/conditional_numbers_final/src/App.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import { db } from "./db";
+
+export default function App() {
+  const [messages, setMessages] = useState<string[]>([
+    db[0],
+  ]);
+  const addMessage = () =>
+    setMessages([...messages, db[messages.length]]);
+  const removeMessage = () =>
+    setMessages(messages.slice(0, messages.length - 1));
+
+  return (
+    <div className="App">
+      <div>
+        <button onClick={addMessage}>Add</button>
+        <button onClick={removeMessage}>Remove</button>
+      </div>
+      {!!messages.length && (
+        <ul>
+          {messages.map((message) => (
+            <li>{message}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
