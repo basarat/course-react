bbadc7ec4dcd0eb3feb936c913486f6f77f39759
basarat <basaratali@gmail.com>
2024-08-30 14:38:26 +1000

    dep

---
+++ lessons/useeffect_dependency/src/App.tsx
@@ -0,0 +1,30 @@
+import styles from './App.module.css';
+
+import { useState, useEffect } from 'react';
+
+/** Mimic a backend API */
+const getWordCount = async (value: string) => {
+  await new Promise(res => setTimeout(res, 1000));
+  return value.split(' ').length;
+}
+
+export default function App() {
+  const [value, setValue] = useState('');
+  const [wordCount, setWordCount] = useState(0);
+
+  useEffect(() => {
+    if (value == '') {
+      setWordCount(0);
+    } else {
+      getWordCount(value)
+        .then(result => setWordCount(result));
+    }
+  }, [value]);
+
+  return (
+    <div className={styles.root}>
+      <input value={value} onChange={(e) => setValue(e.target.value)} />
+      {!!wordCount && <div>Word count: {wordCount}</div>}
+    </div>
+  );
+}
