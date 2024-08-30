1e8ed4ee780d84a249cf8eb6cac5a9da4109a2a3
basarat <basaratali@gmail.com>
2024-08-30 14:11:06 +1000

    another

---
+++ lessons/rules_of_hooks_first/src/App.tsx
@@ -0,0 +1,32 @@
+import styles from "./App.module.css";
+import { useState } from "react";
+
+/**
+ * Hook calls must be made from the top-level of a function component
+ */
+
+// ❌ This will fail because the hook call is not from a function component
+// const [invalid, setInvalid] = useState('Jane');
+
+function useCustom() {
+  // ✅ This is a valid hook call
+  // ∵ useCustom is called from the top-level of a function component
+  const [third, setThird] = useState("Jack");
+  return [third, setThird] as const;
+}
+
+export default function App() {
+  // ✅ This is a valid hook call
+  // ∵ we are at the top-level of the App function component
+  const [first, setFirst] = useState("Jane");
+  const [second, setSecond] = useState("John");
+  const [third, setThird] = useCustom();
+
+  return (
+    <div className={styles.root}>
+      <input value={first} onChange={(e) => setFirst(e.target.value)} />
+      <input value={second} onChange={(e) => setSecond(e.target.value)} />
+      <input value={third} onChange={(e) => setThird(e.target.value)} />
+    </div>
+  );
+}
