cba16e94346035cb221a71b11569c49110654fa2
basarat <basaratali@gmail.com>
2024-09-02 14:30:21 +1000

    better

--- lessons/useref_basic/src/App.tsx
+++ lessons/useref_basic/src/App.tsx
@@ -1,13 +1,15 @@
 import styles from './App.module.css';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 export default function App() {
   const [value, setValue] = useState('');
   const [message, setMessage] = useState('Submit to validate');
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const onSubmit = () => {
     if (!value) {
       setMessage('Please enter a value');
+      inputRef.current?.focus();
     } else {
       setMessage('Validated');
     }
@@ -15,7 +17,7 @@ export default function App() {
 
   return (
     <div className={styles.root}>
-      <input value={value} onChange={(e) => setValue(e.target.value)} />
+      <input ref={inputRef} value={value} onChange={(e) => setValue(e.target.value)} />
       <p className={styles.message}>{message}</p>
       <button onClick={onSubmit}>Submit</button>
     </div>
