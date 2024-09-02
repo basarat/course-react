a7452b993b361e9de41bace94f2537b37a23c339
basarat <basaratali@gmail.com>
2024-09-02 14:46:47 +1000

    perforamnce

---
+++ lessons/useref_performance/src/App.tsx
@@ -0,0 +1,37 @@
+import styles from './App.module.css';
+import { useEffect, useRef, useState } from 'react';
+
+export default function App() {
+  const [seconds, setSeconds] = useState(0);
+
+  // focus(1:1)
+  const intervalRef = useRef<number | null>(null);
+
+  const startInterval = () => {
+    // focus (1:3)
+    intervalRef.current = setInterval(() => {
+      setSeconds((seconds) => seconds + 1);
+    }, 1000);
+  }
+
+  const stopInterval = () => {
+    // focus (1:3)
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current!);
+    }
+  }
+
+  useEffect(() => {
+    // focus(1:1)
+    return stopInterval;
+  }, []);
+
+  // focus(3:4)
+  return (
+    <div className={styles.vertical}>
+      <button onClick={startInterval}>Start</button>
+      <button onClick={stopInterval}>Stop</button>
+      <div>Time: {seconds}s</div>
+    </div>
+  );
+}
