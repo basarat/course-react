1d92d9f5012600493faac25791dc764011deb018
basarat <basaratali@gmail.com>
2024-09-02 14:53:09 +1000

    callback

--- lessons/useref_callback/src/App.tsx
+++ lessons/useref_callback/src/App.tsx
@@ -1,17 +1,14 @@
 import styles from "./App.module.css";
 
-// focus(1:5)
-const logger = {
-  set current(value: HTMLInputElement) {
-    console.log("Setting", value);
-  },
-};
-
 export default function App() {
-  // focus(3:3)
+  // focus(4:6)
   return (
     <div className={styles.root}>
-      <input ref={logger} />
+      <input
+        ref={(element: HTMLInputElement) => {
+          console.log(element);
+        }}
+      />
     </div>
   );
 }
