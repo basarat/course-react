7353723cf8c7392392278738f3de2c68dd7d7996
basarat <basaratali@gmail.com>
2024-09-06 14:17:23 +1000

    expensive

--- lessons/usememo_expensive/src/App.tsx
+++ lessons/usememo_expensive/src/App.tsx
@@ -1,3 +1,35 @@
+import { useState } from "react";
+// focus(1:1)
+import { useMemo } from "react";
+
+// focus(1:3)
+function fibonacci(index: number): number {
+  if (index <= 1) return 1;
+  return fibonacci(index - 1) + fibonacci(index - 2);
+}
+
 export default function App() {
-  return <>Hello Fam!</>;
+  const [name, setName] = useState("");
+  const [num, setNum] = useState(0);
+
+  // focus(1:1)
+  const result = useMemo(() => fibonacci(num), [num]);
+
+  return (
+    <>
+      <input
+        type="number"
+        value={num}
+        onChange={(e) => setNum(Number(e.target.value))}
+      />
+      <div>Result: {result}</div>
+
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <div>Name: {name}</div>
+    </>
+  );
 }
