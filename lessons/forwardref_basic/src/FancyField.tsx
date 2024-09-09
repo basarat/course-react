811d1dfb9f4224d3bfb19a9f4e313f310de727e5
basarat <basaratali@gmail.com>
2024-09-10 09:26:06 +1000

    better

--- lessons/forwardref_basic/src/FancyField.tsx
+++ lessons/forwardref_basic/src/FancyField.tsx
@@ -1,30 +1,36 @@
-import React from 'react';
+import React from "react";
 
 const fieldStyle = {
-  display: 'flex',
-  flex: '1',
-  flexDirection: 'column',
-  gap: '10px',
-  justifyContent: 'center',
+  display: "flex",
+  flex: "1",
+  flexDirection: "column",
+  gap: "10px",
+  justifyContent: "center",
 } as const;
 
 type FancyFieldProps = {
-  id: string,
-  label: string,
-  value: string,
-  onChange: (value: string) => void,
+  id: string;
+  label: string;
+  value: string;
+  onChange: (value: string) => void;
 };
 
-
 export const FancyField =
-  // focus(1:1)
-  React.forwardRef(function FancyField({ id, label, value, onChange }: FancyFieldProps, ref: React.Ref<HTMLInputElement>) {
-    // focus(4:4)    
+  // focus(1:4)
+  React.forwardRef(function FancyField(
+    { id, label, value, onChange }: FancyFieldProps,
+    ref: React.Ref<HTMLInputElement>
+  ) {
+    // focus(4:9)
     return (
       <div style={fieldStyle}>
         <label htmlFor={id}>{label}</label>
-        <input id={id} ref={ref}
-          value={value} onChange={(e) => onChange(e.target.value)} />
+        <input
+          id={id}
+          ref={ref}
+          value={value}
+          onChange={(e) => onChange(e.target.value)}
+        />
       </div>
     );
   });
