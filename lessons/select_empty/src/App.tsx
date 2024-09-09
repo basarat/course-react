37410d1071436084764c477b31e3fe383aacba1e
basarat <basaratali@gmail.com>
2024-09-09 13:00:19 +1000

    empty

---
+++ lessons/select_empty/src/App.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+export default function App() {
+  // Start off with an empty value
+  const [value, setValue] = useState("");
+
+  return (
+    <>
+      <label htmlFor="pet">Choose a pet</label>
+      <select id="pet" value={value} onChange={(e) => setValue(e.target.value)}>
+        {/* Something to display for an empty value */}
+        <option value="">--Please choose an option--</option>
+        <option value="dog">Dog</option>
+        <option value="cat">Cat</option>
+        <option value="parrot">Parrot</option>
+        <option value="chicken">Chicken</option>
+      </select>
+      {/* Display an icon based on the selected value */}
+      {
+        {
+          dog: "🐕",
+          cat: "🐈",
+          parrot: "🦜",
+          chicken: "🐓",
+        }[value]
+      }
+    </>
+  );
+}
