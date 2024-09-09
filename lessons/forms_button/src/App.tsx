7694347bc82dc8649c95608e2dddc50b56ac9853
basarat <basaratali@gmail.com>
2024-09-09 14:32:22 +1000

    forms button

---
+++ lessons/forms_button/src/App.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { Form } from "./Form";
+
+export default function App() {
+  const [username, setUsername] = useState("");
+
+  return (
+    <>
+      <Form
+        onSubmit={() => {
+          alert("Submitted!");
+        }}
+      >
+        <label htmlFor="username">Username</label>
+        <input
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        {/* Ensure that buttons not intended for form submission have `type='button'` */}
+        <div>
+          <button type="button" onClick={() => setUsername("Alpha")}>Alpha</button>
+          <button type="button" onClick={() => setUsername("Beta")}>Beta</button>
+          <button type="button" onClick={() => setUsername("Gamma")}>Gamma</button>
+        </div>
+
+        <button type="submit">Submit</button>
+      </Form>
+    </>
+  );
+}
