403b1ea9b91a69faf90f187a72256a6ab569df39
basarat <basaratali@gmail.com>
2024-09-09 14:00:55 +1000

    better

--- lessons/forms_basic/server/server.js
+++ lessons/forms_basic/server/server.js
@@ -1,19 +1,19 @@
-const http = require('http');
+import http from 'http';
 
 http.createServer((req, res) => {
-  /** CORS */
+	/** CORS */
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader('Access-Control-Request-Method', '*');
 	res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
 	res.setHeader('Access-Control-Allow-Headers', '*');
-	if ( req.method === 'OPTIONS' ) {
+	if (req.method === 'OPTIONS') {
 		res.writeHead(200);
 		res.end();
 		return;
-  }
-  
-  /** Echo */
-  req.pipe(res);
+	}
+
+	/** Echo */
+	req.pipe(res);
 }).listen(8080, () => {
-  console.log('Server is running on port 8080');
+	console.log('Server is running on port 8080');
 });
