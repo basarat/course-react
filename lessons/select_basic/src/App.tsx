cd15fc9795b7569724ec5694dc870813a887e858
basarat <basaratali@gmail.com>
2024-09-09 12:51:46 +1000

    basic

---
+++ lessons/select_basic/src/App.tsx
@@ -0,0 +1,13 @@
+export default function App() {
+  return (
+    <>
+      <label htmlFor="pet">Choose a pet</label>
+      <select id="pet">
+        <option value="dog">Dog</option>
+        <option value="cat">Cat</option>
+        <option value="parrot">Parrot</option>
+        <option value="chicken">Chicken</option>
+      </select>
+    </>
+  );
+}
