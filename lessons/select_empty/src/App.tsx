ad0952d02f0b8ec32d543e1dc3acb6dfa8210e08
basarat <basaratali@gmail.com>
2024-09-09 13:05:00 +1000

    better

--- lessons/select_empty/src/App.tsx
+++ lessons/select_empty/src/App.tsx
@@ -9,7 +9,7 @@ export default function App() {
       <label htmlFor="pet">Choose a pet</label>
       <select id="pet" value={value} onChange={(e) => setValue(e.target.value)}>
         {/* Something to display for an empty value */}
-        <option value="">--Please choose an option--</option>
+        <option value="">None</option>
         <option value="dog">Dog</option>
         <option value="cat">Cat</option>
         <option value="parrot">Parrot</option>
@@ -17,7 +17,6 @@ export default function App() {
       </select>
 
       {{
-        '': '🤷🏻‍♂️',
         'dog': '🐕',
         'cat': '🐈',
         'parrot': '🦜',
