ba875519b4dade71ce9e9c01db078aaacac0d6b3
basarat <basaratali@gmail.com>
2024-09-10 10:39:10 +1000

    wip

---
+++ lessons/useimperativehandle_advanced/src/App.tsx
@@ -0,0 +1,16 @@
+import { useRef } from 'react';
+// focus(1:1)
+import { Handle, CustomComponent } from './CustomComponent';
+
+export default function App() {
+  // focus(1:1)
+  const customComponentRef = useRef<Handle>(null);
+
+  // focus(2:2,4:5)
+  return <>
+    <CustomComponent ref={customComponentRef} />
+
+    <button onClick={() => customComponentRef.current?.logMessage('Sample Log Message')}>Log Message</button>
+    <button onClick={() => customComponentRef.current?.logError('Sample Log Error')}>Log Error</button>
+  </>;
+}
---
+++ lessons/useimperativehandle_advanced/src/CustomComponent.tsx
@@ -0,0 +1,26 @@
+// focus(1:1)
+import { useImperativeHandle } from 'react';
+import { forwardRef } from 'react';
+
+// focus(1:4)
+export type Handle = {
+  logMessage: (value: string) => void,
+  logError: (value: string) => void,
+};
+
+export const CustomComponent = forwardRef(
+  function CustomComponent(props: {}, ref: React.Ref<Handle>) {
+    // focus(1:8)
+    useImperativeHandle(ref, () => ({
+      logMessage: (value: string) => {
+        console.log(value);
+      },
+      logError: (value: string) => {
+        console.error(value);
+      },
+    }));
+    return (
+      <div>Custom Component</div>
+    );
+  }
+);
